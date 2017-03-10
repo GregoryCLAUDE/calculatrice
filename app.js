6f3e99dc67143e0f3cae21d38f181295db82f5dd
Grégory <claudejurado@orange.fr>
2017-03-10 17:12:54 +0100

    debut de la calculatrice, laborieux, mise en place du css et html, debut du javascript

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log ("test");
+
+var button ="";
+
+var val ="";
+
+var tag ="";
+
+function multiplication(a,b){};
+
+function division(a,b){};
+
+function substraction(a,b){};
+
+function addition(a,b){};
+
+$(".calc-but") .onclick ( function())
+{
+	button =$()
+}
+if (button =="=")
+{
+	tag = eval(val);
+};
+else if (button =="CE") {
+	val = 0;
+	tag = 0;
+
+}
+else {
+	val +=
+}
+$(".calc-but") .onclick ( function()){
+if (button == "=")
+{
+	tag = eval(val);
+}
+else if (button =="CE"){
+	val = 0
+} ;
+else {
+	val 
+}
